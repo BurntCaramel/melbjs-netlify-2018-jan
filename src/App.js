7692a1da350f8dcbcb4db5f3fa45e200af9aab9c
Patrick Smith <patrick@burntcaramel.com>
2018-01-10 10:18:40 +1100

    Init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <main className="App">
+        <p className="App-intro">
+          🌜 Evening MelbJS! 🌛
+          <br />
+          ✨ ✨ ✨
+        </p>
+      </main>
+    );
+  }
+}
+
+export default App;
